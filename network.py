93f3b66e96f696bc961ee55d9645329bd5576c77
Stephen Irvine <stephen.irvine@autodesk.com>
2018-02-20 09:25:24 +0000

    Draw results on canvas

--- network.py
+++ network.py
@@ -2,6 +2,7 @@
 from tkinter import Tk, Canvas, Frame, BOTH
 import random
 
+size = 3
 
 class Window(Frame):
 
@@ -21,6 +22,13 @@ class Window(Frame):
     def line(self, start_x, start_y, end_x, end_y): 
         self.canvas.create_line(start_x, start_y, end_x, end_y)
 
+    def circle(self, x, y):
+        self.canvas.create_oval(x-size, y-size, x+size, y+size)
+    
+    def cross(self, x, y):
+        self.canvas.create_line(x-size, y-size, x+size, y+size)
+        self.canvas.create_line(x-size, y+size, x+size, y-size)
+
 
 class Perceptron:
     def __init__(self, a, b):
@@ -34,8 +42,8 @@ class Perceptron:
         s_x = 0
         s_y = 0
         if 0 <= self.b <= 100:
-            s_x = self.b
-            s_y = 0
+            s_x = 0
+            s_y = self.b
         else:
             exit(0)
 
@@ -43,12 +51,31 @@ class Perceptron:
         e_y = 0
         x_intercept = -self.b/self.a
         if 0 < x_intercept < 100:
-            e_x = 0
-            e_y = x_intercept
+            e_x = x_intercept
+            e_y = 0
         else:
             exit(0)
 
         window.line(s_x, s_y, e_x, e_y)
+
+class Result:
+
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+
+    def draw(self, window):
+        if self.value:
+            window.circle(self.x, self.y)
+        else:
+            window.cross(self.x, self.y)
+
+    def text(self):
+        if self.value:
+            return "o"
+        else:
+            return "x"
 
 
 window = Window()
@@ -60,11 +87,15 @@ points_y = [random.randint(0, 100) for i in range(100)]
 results = [None] * 100
 
 for i in range(100):
-    results[i] = p1.classify(points_x[i], points_y[i])
+    result_type = p1.classify(points_x[i], points_y[i])
+    results[i] = Result(points_x[i], points_y[i], result_type)
 
 print("Results: ")
 for i in range(10):
-    print("  ", results[i:i+10])
+    print("  ", list(map(lambda x:x.text(),results[i:i+10])))
+
+for item in results:
+    item.draw(window)
 
 p1.draw(window)
 window.mainloop()
